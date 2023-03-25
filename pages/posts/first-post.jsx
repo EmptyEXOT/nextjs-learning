6334499226a4f7fca0f999a6eeeb026a4e9c8795
EmptyEXOT <sadullaev.alim@mail.ru>
2023-03-26 02:15:51 +0300

    Link component

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,3 +1,12 @@
-export default function FirstPost () {
-    return <h1>First post</h1>
+import Link from "next/link";
+
+export default function FirstPost() {
+    return (
+        <>
+            <h1>First post</h1>
+            <h2>
+                <Link href="/">Back home</Link>
+            </h2>
+        </>
+    )
 }
